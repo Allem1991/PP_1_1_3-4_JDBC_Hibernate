6a79c6844e0a206e12798f68c909dd5ab4bf922c
Kamil <kamilkhusnullin1@gmail.com>
2023-10-16 22:37:10 +0300

    Task 1.1.4 - update3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,13 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import jm.task.core.jdbc.util.Util;
-
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,7 +14,7 @@ public class Main {
         userService.saveUser("Scott", "Smith", (byte) 76);
         userService.saveUser("Amanda", "Plow", (byte) 34);
 
-        System.out.println(userService.getAllUsers());
+        userService.getAllUsers().forEach(System.out::println);
 
         userService.cleanUsersTable();
 
